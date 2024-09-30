01bab9a049fd21d2c2d97d7beb75d1e8030202a3
Haneol99 <haneol.yeomk@gmail.com>
2024-09-29 19:30:36 -0700

    Initial commit

---
+++ Lab15.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+const int W15 = 15;
+
+class Movie
+{
+    private:
+        string movieTiltle;
+        int yearReleased;
+        string screenWriter;
+    
+    public:
+        string getTitle() const {return movieTiltle; }
+        
+
+
+};
+
+int main()
+{
+
+    return 0;
+}
