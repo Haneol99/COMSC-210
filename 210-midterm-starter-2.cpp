7b7b02e269c5565706c10318c4326ed7cace613d
Haneol99 <haneol.yeomk@gmail.com>
2024-10-18 20:09:38 -0700

    Initial commit

--- 210-midterm-starter-2.cpp
+++ 210-midterm-starter-2.cpp
@@ -49,155 +49,24 @@ public:
     void removeFront(){
         if(!head) return;
         Node* temp = head;
-    }
-
-
-
-
-    void insert_after(int value, int position) {
-        if (position < 0) {
-            cout << "Position must be >= 0." << endl;
-            return;
-        }
-
-        Node* newNode = new Node(value);
-        if (!head) {
-            head = tail = newNode;
-            return;
-        }
-
-        Node* temp = head;
-        for (int i = 0; i < position && temp; ++i)
-            temp = temp->next;
-
-        if (!temp) {
-            cout << "Position exceeds list size. Node not inserted.\n";
-            delete newNode;
-            return;
-        }
-
-        newNode->next = temp->next;
-        newNode->prev = temp;
-        if (temp->next)
-            temp->next->prev = newNode;
-        else
-            tail = newNode;
-        temp->next = newNode;
-    }
-
-    void delete_val(int value) {
-        if (!head) return;
-
-        Node* temp = head;
-        
-        while (temp && temp->data != value)
-            temp = temp->next;
-
-        if (!temp) return; 
-
-        if (temp->prev)
-            temp->prev->next = temp->next;
-        else
-            head = temp->next; 
-
-        if (temp->next)
-            temp->next->prev = temp->prev;
-        else
-            tail = temp->prev; 
-
-        delete temp;
-    }
-
-    void delete_pos(int pos) {
-        if (!head) {
-            cout << "List is empty." << endl;
-            return;
-        }
-    
-        if (pos == 1) {
-            pop_front();
-            return;
-        }
-    
-        Node* temp = head;
-    
-        for (int i = 1; i < pos; i++){
-            if (!temp) {
-                cout << "Position doesn't exist." << endl;
-                return;
-            }
-            else
-                temp = temp->next;
-        }
-        if (!temp) {
-            cout << "Position doesn't exist." << endl;
-            return;
-        }
-    
-        if (!temp->next) {
-            pop_back();
-            return;
-        }
-    
-        Node* tempPrev = temp->prev;
-        tempPrev->next = temp->next;
-        temp->next->prev = tempPrev;
-        delete temp;
-    }
-
-    void push_back(int v) {
-        Node* newNode = new Node(v);
-        if (!tail)
-            head = tail = newNode;
-        else {
-            tail->next = newNode;
-            newNode->prev = tail;
-            tail = newNode;
-        }
-    }
-    
-    void push_front(int v) {
-        Node* newNode = new Node(v);
-        if (!head)
-            head = tail = newNode;
-        else {
-            newNode->next = head;
-            head->prev = newNode;
-            head = newNode;
-        }
-    }
-    
-    void pop_front() {
-
-        if (!head) {
-            cout << "List is empty." << endl;
-            return;
-        }
-
-        Node * temp = head;
-
-        if (head->next) {
-            head = head->next;
-            head->prev = nullptr;
-        }
-        else
+        if(head == tail){
             head = tail = nullptr;
+        } else {
+            head = head -> next;
+            head -> prev = nullptr;
+        }
         delete temp;
     }
 
-    void pop_back() {
-        if (!tail) {
-            cout << "List is empty." << endl;
-            return;
-        }
-        Node * temp = tail;
-
-        if (tail->prev) {
-            tail = tail->prev;
-            tail->next = nullptr;
-        }
-        else
+    void removeBack(){
+        if(!tail) return;
+        Node* temp = tail;
+        if(head == tail){
             head = tail = nullptr;
+        } else {
+            tail = tail-> prev;
+            tail-> next = nullptr;
+        }
         delete temp;
     }
 
